a8e775b3f988abd467729e470391d522190bcd0d
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-27 14:23:01 +0200

    Get the api call and set the characters based on the result,added a cards components

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import "./styles.css"
 import "./Components/Navbar.css"
 import Navbar from "./Components/Navbar";
 import { useEffect, useState } from "react";
+import Axios from "axios";
+import CardsContainer from "./Components/CardsContainer";
 
 function App() {
 
   const [characters, setCharacters] = useState("")
+  const [isItemLoading, setIsItemLoading] = useState(false)
 
-  useEffect(() => {
-      fetch('https://rickandmortyapi.com/api')
-      .then(response => response.json())
-      .then()
-  },[])
+ const getApi = () => {
+  Axios.get("https://rickandmortyapi.com/api/character").then((response) => {
+  setCharacters(response.data.results)
+  })
+ }
+
+ useEffect(() => {
+  getApi()
+ },[])
+ 
+ console.log(characters)
 
 
   return (
     <>
     <Navbar />
+    <CardsContainer />
     </>
   );
 }
