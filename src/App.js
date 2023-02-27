efe3c6d8dbfb04e6ad01757ec9a23863220e700b
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-27 13:57:20 +0200

    create Card container and added some base styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-
+import "./styles.css"
+import "./Components/Navbar.css"
+import Navbar from "./Components/Navbar";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [characters, setCharacters] = useState("")
+
+  useEffect(() => {
+      fetch('https://rickandmortyapi.com/api')
+      .then(response => response.json())
+      .then()
+  },[])
+
+
   return (
-    <div>yes</div>
+    <>
+    <Navbar />
+    </>
   );
 }
 
