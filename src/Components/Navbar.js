7ef431930cdbe1172a750e66a48431bf8c65b6ff
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-27 13:40:29 +0200

    Nav bar created and styled

---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+        <div className="title"><h1>Deck Builder</h1></div>
+        
+    </nav>
+  )
+}
