822ee56dff5b55ee34a67a65ad5ce20a78d55d7c
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-27 18:31:59 +0200

    created modal

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,14 @@ export default function Cards(props) {
   return (
     <div className="box" key={props.card.id}    card={props.card} >
       <img  src={props.card.image}  alt="" />
-      <h3>{props.card.name}</h3>
+      <h3>Name: {props.card.name}</h3>
+      <h3> status: {props.card.status}</h3>
+      <h3> Species: {props.card.species}</h3>
+      <h3> Origin: {props.card.origin.name}</h3>
+      <h3> Location: {props.card.location.name}</h3>
+
+
+
     </div>
   )
 }
