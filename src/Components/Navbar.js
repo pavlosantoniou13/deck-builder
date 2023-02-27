f0d5ecf8c58215082dbee0b5b28ff14551089280
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-27 15:17:06 +0200

    added a shuffle button and styled it

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,9 @@ export default function Navbar() {
   return (
     <nav className='navbar'>
         <div className="title"><h1>Deck Builder</h1></div>
-        
+        <div className="Shuffle">
+          <button className='shuffle' >Shuffle</button>
+        </div>
     </nav>
   )
 }
